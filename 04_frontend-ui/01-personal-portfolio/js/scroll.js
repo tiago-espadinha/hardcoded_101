f97c47f67c879e721592f849638ec3f1b5802542
tiago-espadinha <tiago.espadinha.simoes@gmail.com>
2022-02-28 09:25:33 +0000

    feat: add timeline section and scroll animations

---
+++ 04_frontend-ui/01-personal-portfolio/js/scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const reveals = document.querySelectorAll('.reveal');
+
+    const revealOnScroll = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1,
+        rootMargin: '0px 0px -50px 0px'
+    });
+
+    reveals.forEach(reveal => {
+        revealOnScroll.observe(reveal);
+    });
+});
